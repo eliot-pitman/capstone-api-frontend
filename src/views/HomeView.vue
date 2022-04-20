ef8864ba5cb22eb3c0788dcf4924fa597d8ea2d7
Eliot <eliotpitman@gmail.com>
2022-04-19 19:42:27 -0500

    adds getWeather for each favorited airport

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,42 @@
 <script>
 import axios from "axios";
+// import moment from "moment";
 export default {
   data: function () {
     return {
       message: "Welcome to METAR, here are your favorite airports METAR",
       airport: [],
-      token: "",
-      user: [],
       info: [],
       favorites: [],
       favoritesWeather: "",
+      currentFavoriteWeather: [],
+      timer: "",
     };
   },
   created: function () {
-    axios.get("/favorites").then((response) => {
-      this.favorites = response.data;
-      console.log("favorites", response.data);
-    });
+    this.getFavorites();
   },
   methods: {
-    getWeather: function () {
+    getWeather: function (favorite) {
       const headers = {
-        Authorization: "Bearer token(.env)",
+        Authorization: "Bearer lOeb42_xPJibT35Pn9HvjCNI1GWcpsUNW9n0h4gQgw8",
       };
-      axios.get("https://avwx.rest/api/metar/jfk", { headers }).then((response) => {
-        this.user = response.data;
+      axios.get(`https://avwx.rest/api/metar/${favorite}`, { headers }).then((response) => {
+        this.currentFavoriteWeather = response.data;
         console.log("favorite weather front end", response.data);
       });
     },
+    getFavorites: function () {
+      axios.get("/favorites").then((response) => {
+        this.favorites = response.data;
+        console.log("favorites", response.data);
+      });
+    },
+  },
+  mounted: function () {
+    window.setInterval(() => {
+      this.getFavorites();
+    }, 300000);
   },
 };
 </script>
@@ -35,12 +44,13 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <button @click="getWeather">front end test</button>
-    <!-- <div v-for></div> -->
-    <h2>{{ favorites }}</h2>
-    <h1>favorites weather</h1>
-    <div v-for="favoriteW in favoritesWeather" :key="favoriteW"></div>
-    <h2>{{ user }}</h2>
+    <h1>{{ favorites }}</h1>
+    <div v-for="favorite in favorites" :key="favorite.id">
+      <h1>Airport Name: {{ favorite.airport_name }}</h1>
+      <h2>Airport IATA: {{ favorite.airport_iata }}</h2>
+      <h2>Current Airport Weather {{ currentFavoriteWeather }}</h2>
+      <button @click="getWeather(favorite.airport_iata)">update weather</button>
+    </div>
   </div>
 </template>
 
