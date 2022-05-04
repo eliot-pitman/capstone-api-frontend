ceab6448865b89aaa0e1d762d59e36866500ded5
Eliot <eliotpitman@gmail.com>
2022-05-04 01:39:27 -0500

    updates lobby

--- src/views/LobbyMETAR.vue
+++ src/views/LobbyMETAR.vue
@@ -6,8 +6,16 @@ export default {
 };
 </script>
 
-<template id="lobby">
-  <h1>METAR</h1>
-  <h2>All your Favorite Airports in One Place</h2>
-  <h3>Qucikly make an account to search current weather data</h3>
+<template>
+  <header class="py-5">
+    <div class="container px-lg-5">
+      <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
+        <div class="m-4 m-lg-5">
+          <h1 class="display-5 fw-bold">Welcome, to METAR</h1>
+          <h2>All your Favorite Airports in One Place</h2>
+          <p>Easily make an account to search current weather data</p>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
